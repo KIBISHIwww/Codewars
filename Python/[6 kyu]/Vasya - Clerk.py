4a7ce09685313953f74e73f45c1c6fffcd7daae1
KIBISHIwww <48715993+KIBISHIwww@users.noreply.github.com>
2020-11-01 00:46:11 +0800

    Create Vasya - Clerk.py

---
+++ Python/[6 kyu]/Vasya - Clerk.py
@@ -0,0 +1,10 @@
+def tickets(people):
+    n25 = n50 = n100 = 0
+    for e in people:
+        if   e==25            : n25+=1
+        elif e==50            : n25-=1; n50+=1
+        elif e==100 and n50>0 : n25-=1; n50-=1
+        elif e==100 and n50==0: n25-=3
+        if n25<0 or n50<0:
+            return 'NO'
+    return 'YES'
