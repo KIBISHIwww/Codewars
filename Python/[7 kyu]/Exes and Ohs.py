24da7ae360d212f28f0fd1ea2469e36c3438ebf3
KIBISHIwww <48715993+KIBISHIwww@users.noreply.github.com>
2020-11-01 00:32:51 +0800

    Create Exes and Ohs.py

---
+++ Python/[7 kyu]/Exes and Ohs.py
@@ -0,0 +1,10 @@
+def xo(s):
+    ans_o, ans_x = 0, 0
+    for i in s:
+        if i == 'o' or i == 'O':
+            ans_o+=1
+        elif i == 'x' or i == 'X':
+            ans_x+=1
+    return True if ans_o == ans_x or (ans_o == 0 and ans_x == 0) else False
+
+    yesterday
